a38c92de8c822482ed41b1feb791e6088fada3e6
Sakshiideshmane <sakshideshmane04@gmail.com>
2025-03-06 11:05:04 +0530

    Java-logic-development

---
+++ src/programs/Febonacci.java
@@ -0,0 +1,21 @@
+package programs;
+import java.util.Scanner;
+public class Febonacci {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number of terms:");
+		int n= scanner.nextInt();
+		int a= 0, b=1;
+		
+		System.out.println("Fibonacci Serias: " +a+""+b);
+		for(int i=2; i<n; i++) {
+			int next = a + b;
+			System.out.println(""+ next);
+			a=b;
+			b=next;
+		}
+
+	}
+
+}
